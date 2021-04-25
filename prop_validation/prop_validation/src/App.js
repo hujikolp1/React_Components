dc88e566fd2f00aa14d93d1cfb620c8c38c6859f
bhunjimko <harold9188@gmail.com>
2021-04-25 11:11:58 -0700

    import comp

--- prop_validation/prop_validation/src/App.js
+++ prop_validation/prop_validation/src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import PropComp from './PropComp/PropComp'
 
 function App() {
   return (
     <div className="App">
       <h1>Prop Validation In React</h1>
+      <section>
+        <PropComp
+          label='your name'
+          max={36}
+          value={12}
+        />
+      </section>
     </div>
   );
 }
