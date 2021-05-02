de0ddfcae0abfa74521486678437ef8ec081bba0
bhunjimko <harold9188@gmail.com>
2021-05-01 18:16:17 -0700

    add router

--- navroutes/navroutes/src/App.js
+++ navroutes/navroutes/src/App.js
@@ -1,14 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 import React, { Fragment } from "react";
-import { BrowserRouter as Router, Route, Link, Redirect } from "react-router-dom";
+import { BrowserRouter as Router, Route, Link, NavLink, Redirect } from "react-router-dom";
 import First from "./Components/First";
 import Second from "./Components/Second";
-
+import UserHeader from "./Users/UserHeader";
+import UserMain from "./Users/UserMain";
+import GroupHeader from "./Groups/GroupHeader";
+import GroupMain from "./Groups/GroupMain";
 
 function App() {
   return (
     <div className="App">
+
       <Router>
         <section>
           <nav>
@@ -25,6 +29,46 @@ function App() {
           </section>
         </section>
       </Router>
+
+      <Router>
+      <section>
+        <nav>
+          <NavLink
+            exact
+            to="/"
+            style={{ padding: "0 10px" }}
+            activeStyle={{ fontWeight: "bold" }}
+          >
+            Home
+          </NavLink>
+          <NavLink
+            exact
+            to="/Users"
+            style={{ padding: "0 10px" }}
+            activeStyle={{ fontWeight: "bold" }}
+          >
+            Users
+          </NavLink>
+          <NavLink
+            exact
+            to="/Groups"
+            style={{ padding: "0 10px" }}
+            activeStyle={{ fontWeight: "bold" }}
+          >
+            Groups
+          </NavLink>
+        </nav>
+        <header>
+          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route exact path="/Users" component={UserHeader} />
+          <Route exact path="/Groups" component={GroupHeader} />
+        </header>
+        <main>
+          <Route exact path="/Users" component={UserMain} />
+          <Route exact path="/Groups" component={GroupMain} />
+        </main>
+      </section>
+    </Router>
     </div>
   );
 }
