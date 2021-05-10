7d12a1e03f36c7050504ef1cb4d36008e3f20150
unknown <harold9188@gmail.com>
2021-05-09 21:53:17 -0400

    lazy fade spinners suspense

--- lazycomps/lazycomps/src/App.js
+++ lazycomps/lazycomps/src/App.js
@@ -1,6 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Suspense, lazy } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { FadeLoader } from "react-spinners";
+
+const First = lazy(() =>
+  Promise.all([
+    import("./First"),
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    })
+  ]).then(([m]) => m)
+);
+
+const Second = lazy(() =>
+  Promise.all([
+    import("./Second"),
+    new Promise(resolve => {
+      setTimeout(() => {
+        resolve();
+      }, 3000);
+    })
+  ]).then(([m]) => m)
+);
 
 export default function App() {
   const [MyComponent, setMyComponent] = useState(() => () => null);
@@ -11,5 +35,29 @@ export default function App() {
     });
   }, []);
 
-  return <MyComponent />;
+  return (
+    <div>
+      <MyComponent />
+
+      <Router>
+        <section>
+          <nav>
+            <p>
+              <Link to="first">First</Link>
+            </p>
+            <p>
+              <Link to="second">Second</Link>
+            </p>
+          </nav>
+          <section>
+            <Suspense fallback={<FadeLoader color={"lightblue"} size={150} />}>
+              <Route path="/first" component={First} />
+              <Route path="/second" component={Second} />
+            </Suspense>
+          </section>
+        </section>
+      </Router>
+      
+    </div>
+  )
 }
