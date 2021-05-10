97d1455a4483abcc495efbb45880fd8f81635561
unknown <harold9188@gmail.com>
2021-05-09 22:37:45 -0400

    lazy options

--- lazycomps/lazycomps/src/App.js
+++ lazycomps/lazycomps/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect, Suspense, lazy } from "react";
+import React, { useState, useEffect, Suspense, lazy, Fragment } from "react";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import { FadeLoader } from "react-spinners";
 import MyPage from "./MyPage";
@@ -27,8 +27,23 @@ const Second = lazy(() =>
   ]).then(([m]) => m)
 );
 
+const FirstLazy = lazy(() => import("./FirstLazy"));
+const SecondLazy = lazy(() => import("./SecondLazy"));
+
+function ShowComponent({ name }) {
+  switch (name) {
+    case "first":
+      return <First />;
+    case "second":
+      return <Second />;
+    default:
+      return null;
+  }
+}
+
 export default function App() {
   const [MyComponent, setMyComponent] = useState(() => () => null);
+  const [component, setComponent] = useState("");
 
   useEffect(() => {
     import("./FirstComponent").then(module => {
@@ -66,6 +81,20 @@ export default function App() {
           </section>
         </section>
       </Router>
+
+      <Fragment>
+        <label>
+          Load Component:{" "}
+          <select value={component} onChange={e => setComponent(e.target.value)}>
+            <option value="">None</option>
+            <option value="first">FirstLazy</option>
+            <option value="second">SecondLazy</option>
+          </select>
+        </label>
+        <Suspense fallback="loading...">
+          <ShowComponent name={component} />
+        </Suspense>
+      </Fragment>
 
     </div>
   )
---
+++ lazycomps/lazycomps/src/SecondLazy/index.js
@@ -0,0 +1,15 @@
+
+import React, { Fragment } from "react";
+import Four from "./Four";
+import Five from "./Five";
+import Six from "./Six";
+
+export default function Second() {
+  return (
+    <Fragment>
+      <Four />
+      <Five />
+      <Six />
+    </Fragment>
+  );
+}
