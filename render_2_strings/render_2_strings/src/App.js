59a7917493221587b8832219b97ae30671b1c770
unknown <harold9188@gmail.com>
2021-05-17 15:59:16 -0400

    deprecated renderToString

--- render_2_strings/render_2_strings/src/App.js
+++ render_2_strings/render_2_strings/src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react'; 
+import PropTypes from 'prop-types'; 
 
-function App() {
+function App({items}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <text>Render 2 Strings</text>
+      
+      <ul>
+        {items.map(item => {
+          return <li key={item}>{item}</li>
+        })}
+      </ul>
     </div>
   );
 }
-
+App.propTypes = {
+  items: PropTypes.arrayOf(PropTypes.string).isRequired
+}
 export default App;
