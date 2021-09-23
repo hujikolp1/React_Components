c37c9f13a7086cb47605cf3995c3f4d2770fd2a2
bhu <harold9188@gmail.com>
2021-09-23 11:56:36 -0700

    action data to store

---
+++ react_redux/redux_patterns/sendActionDataToStore.js
@@ -0,0 +1,23 @@
+const notesReducer = (state = 'Initial State', action) => {
+  switch(action.type) {
+    case ADD_NOTE:
+      return state = action.text
+    default:
+      return state;
+  }
+};
+
+const ADD_NOTE = 'ADD_NOTE';
+
+const addNoteText = (note) => {
+  return {
+    type: ADD_NOTE,
+    text: note
+  }
+};
+
+const store = Redux.createStore(notesReducer);
+
+console.log(store.getState());
+store.dispatch(addNoteText('Hello!'));
+console.log(store.getState());
